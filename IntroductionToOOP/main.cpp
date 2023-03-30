4a6120cf56a3bf95a9c3d62d4db2cabb9215240e
Lugov_rr81@top-academy.ru <Lugov_rr81@top-academy.ru>
2023-03-31 01:29:19 +0300

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class Point
+{
+
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+};
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	return sqrt(x_distance * x_distance + y_distance * y_distance);
+}
+
+
+
+//#define STRUCT_POINT
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	int a;        //Объявление переменной 'a' типа 'int'
+	Point A;      //Объявление переменной 'A' типа 'Point'
+	//Объявление объекта 'A' типа 'Point'
+	// Создание экземпляра 'A' структуры 'Point'
+	//Instance - экземпляр.
+
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif // STRUCT_POINT
+
+	Point A;
+	A.set_x (2);
+	A.set_y (3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки A до точки B: " << A.distance(B) << endl;
+	cout << "Расстояние от точки B до точки A: " << B.distance(A) << endl;
+	cout << "Расстояние между точками A и B: " << distance(A, B) << endl;
+	cout << "Расстояние между точками B и A: " << distance(B, A) << endl;
+}
+
+/*
+-------------------------------------------------
+. - оператор прямого доступа (Point operator) - для доступа на прямую.
+-> - оператор косвенного доступа (Arrow operator) - для доступа к полям объеката по адресу
+-------------------------------------------------
+*/
